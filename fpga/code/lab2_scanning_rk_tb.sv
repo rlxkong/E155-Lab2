70c214dedd13b54bfc5bfa9d090e197d80fe6630
rlxkong <rkong@g.hmc.edu>
2026-09-16 17:56:00 -0700

    Clearer testbenches

--- fpga/code/lab2_scanning_rk_tb.sv
+++ fpga/code/lab2_scanning_rk_tb.sv
@@ -30,52 +30,17 @@ module lab2_scanning_rk_tb();
   end
 
   initial begin
+	// reset everything
     reset = 0;
     #20; 			// initial set all values to 0
 	reset = 1; 
 	#20; 			// resume the code (one cycle)
+	enable = 0;
+	#20;
+	enable = 1;
+	#20;		
 
-	// enable and reset verification
-		enable = 0;								// set enable to 0 should freeze the code
-        #60000000;                       			
-        assert (row[0])    
-            $display("PASSED! The enable behaves as desired at time: %0t.", $time);
-        else 
-            $error("FAILED! The enable behaves incorrectly at time: %0t.", $time);
-			
-		enable = 1;
-		#120000000;			// wait a cycle for enable to occur (continue counting)
-		assert (row[1])    
-            $display("PASSED! The enable behaves as desired at time: %0t.", $time);
-        else 
-            $error("FAILED! The enable behaves incorrectly at time: %0t.", $time); 
-			
-		enable = 0;
-		#60000000; 			// check that count freezes when enable is low
-		assert (row[1])    
-            $display("PASSED! The enable behaves as desired at time: %0t.", $time);
-        else 
-            $error("FAILED! The enable behaves incorrectly at time: %0t.", $time); 
-		
-		reset = 0;
-		#60000000;			// reset all initial values to 0
-		assert (row[0])  
-            $display("PASSED! The reset behaves as desired at time: %0t.", $time);
-        else 
-            $error("FAILED! The reset behaves incorrectly at time: %0t.", $time);
 
-		enable = 1;
-		#60000000;			// reset all initial values to 0
-		assert (row[0])  
-            $display("PASSED! The enable behaves as desired at time: %0t.", $time);
-        else 
-            $error("FAILED! The enable behaves incorrectly at time: %0t.", $time);
-			
-	// reset verification 
-		reset = 1;
-		#20;			// wait a cycle for reset to occur
-		
-		
 	// row outputs
 		assert ((dut.count < 6000000) & (row[0]))
             $display("PASSED! The led behaves as desired at time: %0t.", $time);
@@ -99,7 +64,36 @@ module lab2_scanning_rk_tb();
             $display("PASSED! The led behaves as desired at time: %0t.", $time);
         else 
             $error("FAILED! The led behaves incorrectly at time: %0t.", $time); 
-		#110000000;  	// 20ns per cycle
+		#250000000;  	// 20ns per cycle
+
+	// reset verification
+
+		reset = 0;
+		#60000000;			// reset all initial values to 0
+		assert (row[0])  
+            $display("PASSED! The reset behaves as desired at time: %0t.", $time);
+        else 
+            $error("FAILED! The reset behaves incorrectly at time: %0t.", $time);
+		
+		reset = 1;
+		#60000000;		// wait a cycle for reset to occur
+		
+
+	// enable verification
+		enable = 0;								// set enable to 0 should freeze the code
+        #180000000;                       			
+        assert (row[0])    
+            $display("PASSED! The enable behaves as desired at time: %0t.", $time);
+        else 
+            $error("FAILED! The enable behaves incorrectly at time: %0t.", $time);
+			
+		enable = 1;
+		#120000000;			// wait a cycle for enable to occur (continue counting)
+		assert (row[1])    
+            $display("PASSED! The enable behaves as desired at time: %0t.", $time);
+        else 
+            $error("FAILED! The enable behaves incorrectly at time: %0t.", $time); 
+
 
 			
     #100 $stop;
